81abb889c566897dd2b369da53a3772bbe4c4639
Elena-star-sys <elenashulgaelena@gmail.com>
2024-07-25 06:59:34 +0300

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,9 @@
+domzad = "Python"
+zadahi = " 12"
+chasi = "1,5"
+x = 1.5
+y = 12
+z = x / y
+print(z)
+vremya = (z)
+print("Курс:" , domzad, ";" , "Количество задач :", zadahi , ";" , "Затрачено часов:",chasi,";","","Время затраченное на выполнение: ", vremya)
